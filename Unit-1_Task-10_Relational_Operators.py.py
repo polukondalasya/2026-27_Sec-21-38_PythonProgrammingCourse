4ba0564ffe70f15487da12c1d6a3dfa582bc801a
polukondalasya <lasyacyber@gmail.com>
2026-09-25 15:07:58 +0530

    Relational_operators

---
+++ Unit-1_Task-10_Relational_Operators.py.py
@@ -0,0 +1,8 @@
+# Python comparision operators exercise
+a=10
+b=20
+result=a>b #greater than comparision operator
+print("result of",a,">",b,"is : ",result)
+result= a<b #less than comparision operator
+print("result of",a,"<",b,"is:",result)
+            
