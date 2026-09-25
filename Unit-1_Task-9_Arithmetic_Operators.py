8cfc53070fcff9bcfccb751b4488b890e8596752
polukondalasya <lasyacyber@gmail.com>
2026-09-25 15:01:47 +0530

    Arithmetic_operators

---
+++ Unit-1_Task-9_Arithmetic_Operators.py
@@ -0,0 +1,15 @@
+# Arithmetic Operators 
+a= int(input("Enter num1: "))
+b= int(input("Enter num2: "))
+# addition operator
+print("addition of a and b is: ",a+b)
+# substraction operator
+print("substraction of a and b is: ",a-b)
+# multiplication operator
+print("multiplication of a and b is: ",a*b)
+# division operator
+print("division of a and b is: ",a/b)
+# modulus operator
+print("modulus of a and b is: ",a%b)
+# floor division operator
+print("floor division of a and b is: ",a//b)
