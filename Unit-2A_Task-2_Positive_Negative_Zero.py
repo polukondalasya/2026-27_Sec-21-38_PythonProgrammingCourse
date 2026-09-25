a14caea74c910529413b67ac3ec961609ea63742
polukondalasya <lasyacyber@gmail.com>
2026-09-25 16:08:02 +0530

    Postive_negative

---
+++ Unit-2A_Task-2_Positive_Negative_Zero.py
@@ -0,0 +1,8 @@
+# Python program to check if a number is positive, negative, or zero
+a=int(input("Enter a number: "))
+if a>0:
+    print("The number is positive.")
+elif a<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
